a82ad8e3448619f0e48f4bd9776021f12fa51cc6
Benoit Mortier <benoit.mortier@opensides.be>
2011-12-04 13:04:50 +0100

    temporary commit for my friend pav5088, the it should work version :)

--- argonaut-fuse/modules/LTSP.pm
+++ argonaut-fuse/modules/LTSP.pm
@@ -42,12 +42,7 @@ our @ISA = ("Exporter");
 
 use constant USEC => 1000000;
 
-my $admin;
-my $password;
 my $server;
-my $cfg_defaults = {
-  'server' => [ \$server, 'localhost' ],
-};
 
 my $log = Log::Handler->get_logger("argonaut-fuse");
 
@@ -55,124 +50,114 @@ sub get_module_info {
   return "Linux Terminal Server Project";
 };
 
-sub get_config_sections {
-  return $cfg_defaults;
-}
-
-# Do everything that is needed, i.e. write the pxelinux.cfg file
 sub get_pxe_config {
-        my ($filename) = shift || return undef;
+  my ($filename) = shift || return undef;
   my $cmdline;
-        my $result = undef;
+  my $result = undef;
 
   my $mac = argonaut_get_mac_pxe($filename);
 
-        # Prepare the ldap handle
-reconnect:
-        return undef if( ! &main::prepare_ldap_handle_retry
-                ( 5 * USEC, -1, 0.5 * USEC, 1.2 ) );
+  # Prepare the ldap handle
+  reconnect:
+  return undef if (! &main::prepare_ldap_handle_retry(5 * USEC, -1, 0.5 * USEC, 1.2));
 
-
-        # Search for the host to examine the FAI state
+        # Search for the host to examine the LTSP entry
         my $mesg = $main::ldap_handle->search(
-                base => "$main::ldap_base",
-                filter => "(&(macAddress=$mac)(objectClass=gotoTerminal))",
-                attrs => [ 'gotoTerminalPath', 'gotoBootKernel',
-                'gotoKernelParameters', 'cn', 'gotoLdapServer' ] );
+            base => "$main::ldap_base",
+            filter => "(&(macAddress=$mac)(objectClass=gotoTerminal))",
+            attrs => [ 
+                'gotoTerminalPath', 'gotoBootKernel',
+                'gotoKernelParameters', 'cn', 'gotoLdapServer' ]);
 
-
-        if( 0 != $mesg->code ) {
-                goto reconnect if( 81 == $mesg->code );
-                $log->warning("$mac - LDAP MAC lookup error $mesg->code: $mesg->error\n");
-
-                return undef;
+        if (0 != $mesg->code) {
+          goto reconnect if (81 == $mesg->code);
+          $log->warning("$mac - LDAP MAC lookup error $mesg->code: $mesg->error\n");
+          return undef;
         }
 
-        my( $entry, $hostname );
+        my ($entry, $hostname);
         if ($mesg->count() == 1) {
-                $entry = ($mesg->entries)[0];
-                $hostname = $entry->get_value( 'cn' );
+          $entry = ($mesg->entries)[0];
+          $hostname = $entry->get_value ('cn');
         } elsif ($mesg->count() == 0) {
-                $log->info("No LTSP configuration for client with MAC ${mac}\n");
-    return undef;
-  } else {
-                $log->warning("$filename - MAC lookup error: too many LDAP results $mesg->count()\n");
-                return undef;
-        }
+            $log->info("No LTSP configuration for client with MAC ${mac}\n");
+            return undef;
+          } else {
+              $log->warning("$filename - MAC lookup error: too many LDAP results $mesg->count()\n");
+              return undef;
+            }
 
+        my $kernel= $entry->get_value ('gotoBootKernel');
+        my $nfsroot = $entry->get_value ('gotoTerminalPath');
+        $cmdline= $entry->get_value ('gotoKernelparameters');
+        my $ldap_srv= $entry->get_value ('gotoLDAPServer');
 
-  my $kernel= $entry->get_value( 'gotoBootKernel' );
-  my $nfsroot = $entry->get_value( 'gotoTerminalPath' );
-  $cmdline= $entry->get_value( 'gotoKernelparameters' );
-  my $ldap_srv= $entry->get_value( 'gotoLDAPServer' );
-
-  # Check group
+        # Check group
         my $host_dn = $entry->dn;
 
         # If any of these values isn't provided by the client check group membership
-        if( (! defined $kernel)   || ("" eq $kernel)  ||
-                (! defined $cmdline)  || ("" eq $cmdline) ||
-                (! defined $nfsroot)  || ("" eq $nfsroot) ||
-                (! defined $ldap_srv) || ("" eq $ldap_srv)
-        ) {
-                $log->warning("$filename - Information for PXE creation is missing\n");
-                $log->warning("$filename - Checking group membership...\n");
+        if ((! defined $kernel) || ("" eq $kernel) ||
+            (! defined $cmdline)  || ("" eq $cmdline) ||
+            (! defined $nfsroot)  || ("" eq $nfsroot) ||
+            (! defined $ldap_srv) || ("" eq $ldap_srv)) {
+              $log->warning("$filename - Information for PXE creation is missing\n");
+              $log->warning("$filename - Checking group membership...\n");
 
-                my $filter = '(&(member=' . escape_filter_value($host_dn) . ')'
+              my $filter = '(&(member=' . escape_filter_value($host_dn) . ')'
                 . '(objectClass=gosaGroupOfNames)'
                 . '(gosaGroupObjects=[T]))';
-                $mesg = $main::ldap_handle->search(
-                        base => $main::ldap_base,
-                        filter => $filter,
-                        attrs => [ 'gotoBootKernel', 'gotoKernelParameters',
-                        'gotoLdapServer', 'cn', 'gotoTerminalPath' ]);
-                if( 0 != $mesg->code ) {
-                        goto reconnect if( 81 == $mesg->code );
-                        $log->error( sprintf("$filename - LDAP group lookup error $mesg->code: $mesg->error\n");
+              $mesg = $main::ldap_handle->search(
+                base => $main::ldap_base,
+                filter => $filter,
+                attrs => ['gotoBootKernel', 'gotoKernelParameters',
+                        'gotoLdapServer', 'cn', 'gotoTerminalPath']);
+                if (0 != $mesg->code) {
+                        goto reconnect if (81 == $mesg->code);
+                        $log->error("$filename - LDAP group lookup error $mesg->code: $mesg->error\n");
                         return undef;
                 }
 
                 # Get information from group membership
                 my $group_entry;
-                if( 1 == $mesg->count ) {
-                        $group_entry = ($mesg->entries)[0];
-                        $kernel = $group_entry->get_value( 'gotoBootKernel' )
-                        if( ! defined $kernel );
-                        $cmdline = $group_entry->get_value( 'gotoKernelParameters' )
-                        if( ! defined $cmdline );
-                        $ldap_srv = $group_entry->get_value( 'gotoLdapServer' )
-                        if( ! defined $ldap_srv );
-                        $nfsroot = $group_entry->get_value( 'gotoTerminalPath' )
-                        if( ! defined $nfsroot );
+                if (1 == $mesg->count) {
+                  $group_entry = ($mesg->entries)[0];
+                  $kernel = $group_entry->get_value('gotoBootKernel')
+                  if (! defined $kernel);
+                    $cmdline = $group_entry->get_value('gotoKernelParameters')
+                    if (! defined $cmdline);
+                        $ldap_srv = $group_entry->get_value('gotoLdapServer')
+                        if (! defined $ldap_srv);
+                        $nfsroot = $group_entry->get_value('gotoTerminalPath')
+                        if (! defined $nfsroot);
                 }
 
                 # Check, if there is still missing information
-                if( ! defined $cmdline || ! defined $kernel || ! defined $ldap_srv || ! defined $nfsroot ) {
-                        my $single_log;
-                        if ($mesg->count == 0){
-                                $single_log = "$filename - no group membership found - aborting\n";
-                                $log->error($single_log);
-                        } elsif ($mesg->count == 1) {
-                                $single_log = "$filename - missing information in group - aborting\n";
-                                $log->error($single_log);
-                        } else {
-                                $single_log = "$filename - multiple group memberships found "
-                                . "($mesg->count) - aborting!\n";
-                                $log->error($single_log);
-                                foreach $group_entry ($mesg->entries) {
-                                        $log->info("$filename - $group_entry->get_value('cn') - $group_entry->dn()\n");
-                                }
+                if (! defined $cmdline || ! defined $kernel || ! defined $ldap_srv || ! defined $nfsroot ) {
+                  my $single_log;
+                  if ($mesg->count == 0){
+                    $single_log = "$filename - no group membership found - aborting\n";
+                    $log->error($single_log);
+                  } elsif ($mesg->count == 1) {
+                      $single_log = "$filename - missing information in group - aborting\n";
+                      $log->error($single_log);
+                    } else {
+                        $single_log = "$filename - multiple group memberships found "
+                          . "($mesg->count) - aborting!\n";
+                        $log->error($single_log);
+                        foreach $group_entry ($mesg->entries) {
+                          $log->info("$filename - $group_entry->get_value('cn') - $group_entry->dn()\n");
                         }
+                      }
 
-                        $mesg  = "$filename - missing LDAP attribs:";
-                        $mesg .= ' gotoBootKernel' if( ! defined $kernel );
-                        $mesg .= ' gotoKernelParameters' if( ! defined $cmdline );
-                        $mesg .= ' gotoLdapServer' if( ! defined $ldap_srv );
-                        $mesg .= ' gotoTerminalPath' if( ! defined $nfsroot );
-                        $mesg .= "\n";
+                      $mesg  = "$filename - missing LDAP attribs:";
+                      $mesg .= ' gotoBootKernel' if( ! defined $kernel );
+                      $mesg .= ' gotoKernelParameters' if( ! defined $cmdline );
+                      $mesg .= ' gotoLdapServer' if( ! defined $ldap_srv );
+                      $mesg .= ' gotoTerminalPath' if( ! defined $nfsroot );
+                      $mesg .= "\n";
 
-                        $log->warning($mesg);
-                        return undef;
+                      $log->warning($mesg);
+                      return undef;
                 }
         }
 
@@ -185,39 +170,45 @@ reconnect:
   if (not defined $nfsroot) {
     $nfsroot= "";
   } else {
-    # Transform to IP if possible
-    my $server= $nfsroot;
-    my $path= $nfsroot;
-    $server =~ s/:.*$//;
-    $path =~ s/^[^:]+://;
-    if ($server !~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ ){
-      $server = inet_ntoa(inet_aton($server));
+      # Transform to IP if possible
+      my $server= $nfsroot;
+      my $path= $nfsroot;
+      $server =~ s/:.*$//;
+      $path =~ s/^[^:]+://;
+      if ($server !~ /^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$/ ){
+        $server = inet_ntoa(inet_aton($server));
+      }
+
+      $nfsroot= "nfsroot=$server:$path";
     }
 
-    $nfsroot= "nfsroot=$server:$path";
-  }
+    # Set kernel parameters
+    if (not defined $cmdline) {
+      $cmdline= "";
+    }
 
-  # Set kernel parameters
-  if (not defined $cmdline) {
-    $cmdline= "";
-  }
+    # Assign commandline
+    $cmdline = "ro initrd=$initrd ip=dhcp boot=nfs root=/dev/nfs $nfsroot $cmdline";
 
-  # Assign commandline
-  $cmdline = "ro initrd=$initrd ip=dhcp boot=nfs root=/dev/nfs $nfsroot $cmdline";
+    $log->debug("Kernel ($kernel) $cmdline\n");
 
-  $log->debug("Kernel ($kernel) $cmdline\n");
+    $log->info("$filename - PXE status: boot\n");
+    my $code = &main::write_pxe_config_file( undef, $filename, "kernel $kernel", $cmdline );
+    
+    if ($code == 0) {
+      return time;
+    } 
+    
+    if ($code == -1) {
+      $log->error("$filename - unknown error\n");
+    }
 
-  $log->info("$filename - PXE status: boot\n");
-  my $code = &main::write_pxe_config_file( undef, $filename, "kernel $kernel", $cmdline );
-  if ( $code == 0) {
-    return time;
-  } 
-  if ( $code == -1) {
-    $log->error("$filename - unknown error\n");
-  }
-
-  # Return our result
-  return $result;
+    # Return our result
+    return $result;
 }
 
 1;
+
+__END__
+
+# vim:ts=2:sw=2:expandtab:shiftwidth=2:syntax:paste
--- argonaut-fuse/modules/OPSI.pm
+++ argonaut-fuse/modules/OPSI.pm
@@ -40,18 +40,9 @@ use Argonaut::Common qw(:file);
 use Exporter;
 our @ISA = ("Exporter");
 
-my $admin;
-my $password;
-my $server;
-my $lang;
 my $sclient="";
 
-my $cfg_defaults = {
-  'admin'  => [ \$admin,  'admin' ],
-  'password'  => [ \$password,  '' ],
-  'server' => [ \$server, 'localhost' ],
-  'lang' => [ \$lang, 'de' ],
-};
+my ($admin, $password, $server, $lang);
 
 my $log = Log::Handler->get_logger("argonaut-fuse");
 
@@ -59,13 +50,7 @@ sub get_module_info {
   return "Automatic Windows Installation";
 };
 
-sub get_config_sections {
-  return $cfg_defaults;
-}
-
-# Do everything that is needed, i.e. write the pxelinux.cfg file
 sub get_pxe_config {
-# Initialize opsi stuff
   my $opsi_url= "https://$admin:$password\@$server:4447/rpc";
   my $opsi_client = new JSON::RPC::Client;
 
@@ -87,81 +72,81 @@ sub get_pxe_config {
   my $cmdline;
   my $product= "";
 
-  if($res) {
-    if($res->is_error) {
+  if ($res) {
+    if ($res->is_error) {
       $log->error("ch $$: Error : $res->error_message\n");
     } else {
       $sclient=$res->result;
       $callobj = { method  => 'getNetBootProductStates_hash', params  => [ $sclient ], id  => 2, };
       my $res2 = $opsi_client->call($opsi_url, $callobj);
-      if($res2) {
-        if($res2->is_error) {
+      if ($res2) {
+        if ($res2->is_error) {
           $log->error("ch $$: Error : ". $res2->error_message."\n");
         } else {
-          foreach my $element (@{$res2->result->{$sclient}}){
-          if(
-            $element->{'actionRequest'} ne '' &&
-            $element->{'actionRequest'} ne 'undefined' &&
-            $element->{'actionRequest'} ne 'none' ) 
-          {
-            $state= 1;
-            $status= "install";
-            $product= "product=".$element->{'productId'};
-            last;
+            foreach my $element (@{$res2->result->{$sclient}}){
+            if(
+              $element->{'actionRequest'} ne '' &&
+              $element->{'actionRequest'} ne 'undefined' &&
+              $element->{'actionRequest'} ne 'none' ) {
+                $state= 1;
+                $status= "install";
+                $product= "product=".$element->{'productId'};
+                last;
+              }
+            }
           }
+      } else {
+        $log->error("ch $$: Error : $opsi_client->status_line\n");
+      }
+
+      if ($state) {
+        # Installation requested
+        my $service= "";
+        my $pckey= "";
+
+        # Load pc key
+        $callobj = { method  => 'getOpsiHostKey', params  => [ $sclient ], id  => 4, };
+        $res = $opsi_client->call($opsi_url, $callobj);
+        if (defined $res->result) {
+          $pckey= "pckey=".$res->result;
+          $log->info("setting pckey for $sclient\n");
+        } else {
+          $log->warning("no pc key for $sclient found\n");
         }
-      }
-    } else {
-      $log->error("ch $$: Error : $opsi_client->status_line\n");
-    }
 
-    if ( $state ) {
-      # Installation requested
-      my $service= "";
-      my $pckey= "";
-
-      # Load pc key
-      $callobj = { method  => 'getOpsiHostKey', params  => [ $sclient ], id  => 4, };
-      $res = $opsi_client->call($opsi_url, $callobj);
-      if (defined $res->result){
-        $pckey= "pckey=".$res->result;
-        $log->info("setting pckey for $sclient\n");
+        # Load depot server for this client
+        $callobj = { method  => 'getDepotId', params  => [ $sclient ], id  => 5, };
+        $res = $opsi_client->call($opsi_url, $callobj);
+        if (defined $res->result){
+          $service= "service=".$res->result;
+          $log->info("setting depot server for $sclient to $service\n");
+        } else {
+          $log->info("no depot server for $sclient defined\n");
+        }
+        $cmdline = "noapic lang=$lang ramdisk_size=175112 init=/etc/init initrd=opsi-root.gz reboot=b video=vesa:ywrap,mtrr $service $pckey vga=791 quiet splash $product";
       } else {
-        $log->warning("no pc key for $sclient found\n");
+        # Localboot
+        $kernel = 'localboot 0';
+        $cmdline = '';
       }
-
-      # Load depot server for this client
-      $callobj = { method  => 'getDepotId', params  => [ $sclient ], id  => 5, };
-      $res = $opsi_client->call($opsi_url, $callobj);
-      if (defined $res->result){
-        $service= "service=".$res->result;
-        $log->info("setting depot server for $sclient to $service\n");
-      } else {
-        $log->info("no depot server for $sclient defined\n");
-      }
-      $cmdline = "noapic lang=$lang ramdisk_size=175112 init=/etc/init initrd=opsi-root.gz reboot=b video=vesa:ywrap,mtrr $service $pckey vga=791 quiet splash $product";
-    } else {
-      # Localboot
-      $kernel = 'localboot 0';
-      $cmdline = '';
     }
+  } else {
+    $log->error("ch $$: Error : $opsi_client->status_line\n");
   }
-} else {
-  $log->error("ch $$: Error : $opsi_client->status_line\n");
-}
 
 
-$log->info("$filename - PXE status: $status\n");
-my $code = &main::write_pxe_config_file( $sclient, $filename, $kernel, $cmdline );
-if ( $code == 0) {
-  return time;
-} 
-if ( $code == -1) {
-  $log->info("$filename - unknown state: $status\n");
-}
+  $log->info("$filename - PXE status: $status\n");
+  my $code = &main::write_pxe_config_file( $sclient, $filename, $kernel, $cmdline );
+  if ($code == 0) {
+    return time;
+  }
+   
+  if ($code == -1) {
+    $log->info("$filename - unknown state: $status\n");
+  }
 
-# Return our result
-return $result;
+  # Return our result
+  return $result;
 }
 
 1;
