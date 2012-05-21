62e8ee8f536df0b2e57fe5d0ca3c26274c552016
Benoit Mortier <benoit.mortier@opensides.be>
2012-05-21 16:20:38 +0200

    Fixes: #763 The code for applying should go trought argonaut

--- argonaut-quota/Argonaut/Quota.pm
+++ argonaut-quota/Argonaut/Quota.pm
@@ -38,7 +38,7 @@ BEGIN
   $VERSION = '2012-04-24';
   @ISA = qw(Exporter);
 
-  @EXPORT_OK = qw(write_quota_files get_quota_settings apply_users_quota apply_groups_quota);
+  @EXPORT_OK = qw(write_warnquota_file write_quotatab_file get_quota_settings apply_quotas);
 }
 
 =head1
@@ -46,11 +46,10 @@ Warnquota
 
 Write warnquota and quotatab files
 =cut
-sub write_quota_files {
-  my ($settings,$warnquota_file,$quotatab_file) = @_;
+sub write_warnquota_file {
+  my ($settings,$warnquota_file) = @_;
 
   open (WARNQUOTA, ">", $warnquota_file) or die "Could not open file $warnquota_file";
-  open (QUOTATAB, ">", $quotatab_file) or die "Could not open file $quotatab_file";
 
   # edition of warnquota.conf
   print WARNQUOTA "MAIL_CMD               = ".$settings->{'mail_cmd'}."\n";
@@ -76,6 +75,12 @@ sub write_quota_files {
   print WARNQUOTA "LDAP_USER_DN           = ".$settings->{'ldap_userdn'}."\n";
   print WARNQUOTA "LDAP_PASSWORD          = ".$settings->{'ldap_userpwd'}."\n";
   # end of warnquota.conf
+}
+
+sub write_quotatab_file {
+  my ($settings,$quotatab_file) = @_;
+
+  open (QUOTATAB, ">", $quotatab_file) or die "Could not open file $quotatab_file";
 
   # Begin of quota tab edition
   my @quotaDeviceParameters = @{$settings->{'device_parameters'}};
@@ -156,7 +161,7 @@ sub get_quota_settings {
   return $settings;
 }
 
-sub apply_users_quota {
+sub apply_quotas {
   my ($ldap_configfile,$ldap_dn,$ldap_password,$hostname) = @_;
 
   my $ldapinfos = argonaut_ldap_init ($ldap_configfile, 0, $ldap_dn, 0, $ldap_password);
@@ -169,51 +174,29 @@ sub apply_users_quota {
 
   my $mesg = $ldap->search( # perform a search
             base   => $ldap_base,
-            filter => "(&(objectClass=systemQuotas)(uid=*))",
-            attrs => ['quota','uidNumber']
+            filter => "(objectClass=systemQuotas)",
+            attrs => ['quota','uid','uidNumber','gidNumber']
             );
 
   foreach my $entry ($mesg->entries) {
     my $uid = $entry->get_value("uidNumber");
-    my @quotas = $entry->get_value("quota");
-    foreach my $quota (@quotas) {
-      my ($dev,$blocksoft,$blockhard,$inodesoft,$inodehard,$server,$adminlist) = split (':',$quota);
-      if ($server eq $hostname) {
-        print "applying quota ($blocksoft, $blockhard, $inodesoft, $inodehard) on $dev for uid $uid\n";
-        Quota::setqlim($dev, $uid, $blocksoft,$blockhard, $inodesoft,$inodehard);
-      }
-    }
-  }
-}
-
-sub apply_groups_quota {
-  my ($ldap_configfile,$ldap_dn,$ldap_password,$hostname) = @_;
-
-  my $ldapinfos = argonaut_ldap_init ($ldap_configfile, 0, $ldap_dn, 0, $ldap_password);
-
-  if ( $ldapinfos->{'ERROR'} > 0) {
-    die $ldapinfos->{'ERRORMSG'}."\n";
-  }
-
-  my ($ldap,$ldap_base) = ($ldapinfos->{'HANDLE'},$ldapinfos->{'BASE'});
-
-  my $mesg = $ldap->search( # perform a search
-            base   => $ldap_base,
-            filter => "(&(objectClass=systemQuotas)(objectClass=posixGroup))",
-            attrs => ['quota','gidNumber']
-            );
-
-  foreach my $entry ($mesg->entries) {
     my $gid = $entry->get_value("gidNumber");
+    my $isUser = (defined $entry->get_value("uid"));
     my @quotas = $entry->get_value("quota");
     foreach my $quota (@quotas) {
       my ($dev,$blocksoft,$blockhard,$inodesoft,$inodehard,$server,$adminlist) = split (':',$quota);
       if ($server eq $hostname) {
-        print "applying quota ($blocksoft, $blockhard, $inodesoft, $inodehard) on $dev for gid $gid\n";
-        Quota::setqlim($dev, $gid, $blocksoft, $blockhard, $inodesoft, $inodehard, 0, 1);
+        if ($isUser) {
+          print "applying quota ($blocksoft, $blockhard, $inodesoft, $inodehard) on $dev for uid $uid\n";
+          Quota::setqlim($dev, $uid, $blocksoft,$blockhard, $inodesoft,$inodehard);
+        } else {
+          print "applying quota ($blocksoft, $blockhard, $inodesoft, $inodehard) on $dev for gid $gid\n";
+          Quota::setqlim($dev, $gid, $blocksoft, $blockhard, $inodesoft, $inodehard, 0, 1);
+        }
       }
     }
   }
+  Quota::sync();
 }
 
 END {}
