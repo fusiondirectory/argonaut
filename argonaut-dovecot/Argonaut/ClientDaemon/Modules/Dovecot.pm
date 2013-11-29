4d7b19d96e153806ded01001f6a2f22d68082821
Benoit Mortier <benoit.mortier@opensides.be>
2013-11-29 14:20:03 +0100

    Fixes: #2889 cleanup copyright and manpages

--- argonaut-dovecot/Argonaut/ClientDaemon/Modules/Dovecot.pm
+++ argonaut-dovecot/Argonaut/ClientDaemon/Modules/Dovecot.pm
@@ -4,6 +4,8 @@
 #
 # Copyright (C) 2013 FusionDirectory project <contact@fusiondirectory.org>
 #
+# Author: Come Bernigaud
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
